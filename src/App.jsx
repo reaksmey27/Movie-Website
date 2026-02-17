9489ead832d1648e9372f45fda03307b922facfa
REAKSMEY.SAN <REAKSMEY.SAN@PNC.LOCAL>
2026-02-17 12:57:12 +0700

    feat: add home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import React from "react";
-// 1. Fixed Layout Imports
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/footer";
-import MovieContent from "./features/home/HomePage";
-import MovieSlider from "./features/movies/MovieSlider";
-import { TopTrending } from "./constants";
+import Footer from "./components/layout/Footer";
+import HomePage from "./features/home/HomePage";
 
 const App = () => {
-  const actionMovies = TopTrending.filter((m) => m.genres.includes("Action"));
-  const sciFiMovies = TopTrending.filter((m) => m.genres.includes("Sci-Fi"));
-  const comedyMovies = TopTrending.filter((m) => m.genres.includes("Comedy"));
-  const animationMovies = TopTrending.filter((m) => m.genres.includes("Animation"));
-  const crimeMovies = TopTrending.filter((m) => m.genres.includes("Crime"));
-  const fantasyMovies = TopTrending.filter((m) => m.genres.includes("Fantasy"));
 
   return (
     <div className="min-h-screen bg-slate-950 text-white selection:bg-purple-500/30">
       <Navbar />
       <main className="relative">
-        <MovieContent />
-        <div className="relative z-30 mt-10 pb-20 space-y-12">
-          <MovieSlider title="Trending Now" movies={TopTrending} />
-          <MovieSlider title="Sci-Fi" movies={sciFiMovies} />
-          <MovieSlider title="Action" movies={actionMovies} />
-          <MovieSlider title="Comedy" movies={comedyMovies} />
-          <MovieSlider title="Animation" movies={animationMovies} />
-          <MovieSlider title="Crime" movies={crimeMovies} />
-          <MovieSlider title="Fantasy" movies={fantasyMovies} />
-        </div>
+        <HomePage />
       </main>
       <Footer />
     </div>
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import HeroSection from './HeroSection'; 
+import MovieSlider from './MovieSlider';
+import { TopTrending } from '../../constants';
 
-const MovieContent = () => {
+const HomePage = () => {
+  const actionMovies = TopTrending.filter((m) => m.genres.includes("Action"));
+  const sciFiMovies = TopTrending.filter((m) => m.genres.includes("Sci-Fi"));
+  const comedyMovies = TopTrending.filter((m) => m.genres.includes("Comedy"));
+  const animationMovies = TopTrending.filter((m) => m.genres.includes("Animation"));
+  const crimeMovies = TopTrending.filter((m) => m.genres.includes("Crime"));
+  const fantasyMovies = TopTrending.filter((m) => m.genres.includes("Fantasy"));
+
   return (
-    <>
+    <div id="home">
         <HeroSection />
-    </>
+        <div className="relative z-30 mt-10 pb-20 space-y-12">
+          <MovieSlider title="Trending Now" movies={TopTrending} />
+          <MovieSlider title="Sci-Fi" movies={sciFiMovies} />
+          <MovieSlider title="Action" movies={actionMovies} />
+          <MovieSlider title="Comedy" movies={comedyMovies} />
+          <MovieSlider title="Animation" movies={animationMovies} />
+          <MovieSlider title="Crime" movies={crimeMovies} />
+          <MovieSlider title="Fantasy" movies={fantasyMovies} />
+        </div>
+    </div>
   )
 }
 
-export default MovieContent
+export default HomePage
