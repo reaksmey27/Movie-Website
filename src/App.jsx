769db5f999304fb19ea9f295d437f064ac870f0c
REAKSMEY.SAN <REAKSMEY.SAN@PNC.LOCAL>
2026-02-17 11:05:43 +0700

    feat: add homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+// 1. Fixed Layout Imports
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/footer";
+import MovieContent from "./features/home/HomePage";
+import MovieSlider from "./features/movies/MovieSlider";
+import { TopTrending } from "./constants";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const actionMovies = TopTrending.filter((m) => m.genres.includes("Action"));
+  const sciFiMovies = TopTrending.filter((m) => m.genres.includes("Sci-Fi"));
+  const comedyMovies = TopTrending.filter((m) => m.genres.includes("Comedy"));
+  const animationMovies = TopTrending.filter((m) => m.genres.includes("Animation"));
+  const crimeMovies = TopTrending.filter((m) => m.genres.includes("Crime"));
+  const fantasyMovies = TopTrending.filter((m) => m.genres.includes("Fantasy"));
 
-export default App
+  return (
+    <div className="min-h-screen bg-slate-950 text-white selection:bg-purple-500/30">
+      <Navbar />
+      <main className="relative">
+        <MovieContent />
+        <div className="relative z-30 mt-10 pb-20 space-y-12">
+          <MovieSlider title="Trending Now" movies={TopTrending} />
+          <MovieSlider title="Sci-Fi" movies={sciFiMovies} />
+          <MovieSlider title="Action" movies={actionMovies} />
+          <MovieSlider title="Comedy" movies={comedyMovies} />
+          <MovieSlider title="Animation" movies={animationMovies} />
+          <MovieSlider title="Crime" movies={crimeMovies} />
+          <MovieSlider title="Fantasy" movies={fantasyMovies} />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './HeroSection'; 
+
+const MovieContent = () => {
+  return (
+    <>
+        <HeroSection />
+    </>
+  )
+}
+
+export default MovieContent
