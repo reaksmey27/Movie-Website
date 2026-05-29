8951226d0f5bafb54e3016e0d28dfd477ae57934
REAKSMEY.SAN <REAKSMEY.SAN@pnc.local>
2026-05-29 20:31:28 +0700

    feat: add donation modal with QR code payment support

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", 
+
   plugins: [
     react(),
-    tailwindcss(),  
+    tailwindcss(),
   ],
+
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
-})
+});
